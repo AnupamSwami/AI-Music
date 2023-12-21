1c6ec7a150365f8868cff3a22e53830aba380f22
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-12-21 18:51:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function setup(){
-    canvas = createCanvas(600,530);
-    canvas.center();
+song1 = "" ;
+song2 = "" ;
+
+function preload() {
+    song1 = loadSound("song1.mp3") ;
+    song2 = loadSound("song2.mp3") ;
+}
+
+function setup() {
+    canvas = createCanvas(425,300) ;
+    canvas.position(500,200) ;
+    video = createCapture(VIDEO) ;
+    video.size(400,300) ;
+    video.hide() ;
+}
+
+function draw() {
+    background('white') ;
+    image(video , 0 , 0 , 425 , 300) ;
+}
+
+function play() {
+    song.play() ;
 }
