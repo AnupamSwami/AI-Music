6bad07645d4902af538cf6ce3d03ad70f4c56a06
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-12-27 18:56:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0 ;
 leftWristY = 0 ;
 rightWristX = 0 ;
 rightWristY = 0 ;
+scoreLeftWrist = 0 ;
+songStatus1 = "" ;
+songStatus2 = "" ;
 
 function preload() {
     song1 = loadSound("song1.mp3") ;
@@ -28,15 +31,25 @@ function modelLoaded() {
 function draw() {
     background('white') ;
     image(video , 0 , 0 , 425 , 300) ;
-}
+    fill('#F94449') ;
+    stroke('#F94449') ;
 
-function play() {
-    song.play() ;
+    if(scoreLeftWrist > 0.2) {
+        songStatus1 = song1.isPlaying() ;
+        circle(leftWristX , leftWristY , 20) ;
+        song2.stop() ;
+    } if(songStatus1 = "false") {
+        song1.play() ;
+        document.getElementById("song_name").innerHTML = "Song Name - Jaya Kishori Song" ;
+    }
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results) ;
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score ;
+        console.log("scoreLeftWrist = " + scoreLeftWrist) ;
 
         leftWristX = results[0].pose.leftWrist.x ;
         leftWristY = results[0].pose.leftWrist.y ;
@@ -46,4 +59,8 @@ function gotPoses(results) {
         rightWristY = results[0].pose.rightWrist.y ;
         console.log("Right Wrist X = " + rightWristX + " Right Wrist Y = " + rightWristY) ;
     }
+}
+
+function play() {
+    song.play() ;
 }
